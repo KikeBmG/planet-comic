d28ecdd24bb2b4b24c51621ba31339a17bc7ddeb
Quique Gómez <kikebmg@gmail.com>
2023-02-18 09:21:11 +0100

    feat(): scaffold

--- app/utils/langMixin.js
+++ app/utils/langMixin.js
@@ -1,3 +1,11 @@
+import texts from '../locales/locales.json' assert { type: "json" };
+
 export class LangMixin {
-    
+  constructor() {
+    this.lang = window.AppConfig.lang;
+  }
+
+  t(key) {
+    return texts[this.lang][key] || key;
+  }
 }
